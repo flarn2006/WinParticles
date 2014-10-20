04c4e6b3d5c4c0d337e2c6b67d1a8ff1be7cf8cf
flarn2006 <flarn2006@gmail.com>
2014-10-20 02:20:38 -0400

    Bitmap data is now saved in presets.

--- WinParticles/PresetManager.cpp
+++ WinParticles/PresetManager.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <sstream>
 
+extern HDC particleBmpDC;
+
 CPresetManager::CPresetManager(CParticleSys *psys)
 {
 	this->psys = psys;
@@ -87,6 +89,15 @@ bool CPresetManager::SavePreset(LPCTSTR filename)
 		file << "GradientStep=" << gradient->GetStepPosition(i) << "," << std::hex << gradient->GetStepColor(i) << std::endl;
 	}
 
+	file << std::endl;
+	file << "StartBitmap" << std::endl;
+	for (int y = 0; y < 5; y++) {
+		for (int x = 0; x < 30; x++) {
+			file << (GetPixel(particleBmpDC, x, y) > 0 ? '_' : '#');
+		}
+		file << std::endl;
+	}
+
 	file.close();
 	return true;
 }
@@ -98,109 +109,126 @@ bool CPresetManager::LoadPreset(LPCTSTR filename)
 	std::ifstream file(filename);
 	if (file.bad()) return false;
 
+	int bitmapRow = -1; //stores current row of bitmap being read; -1 means it's not a bitmap line
 	std::string line;
 	while (std::getline(file, line)) {
-		std::string *left;
-		std::string *right;
+		if (line.compare("StartBitmap") == 0) {
+			bitmapRow = 0;
+		} else if (bitmapRow == -1) {
+			std::string *left;
+			std::string *right;
 
-		if (SplitString(line, '=', &left, &right)) {
-			std::istringstream parseRight(*right);
+			if (SplitString(line, '=', &left, &right)) {
+				std::istringstream parseRight(*right);
 
-			if (left->compare("VelocityMode") == 0) {
-				if (right->compare("POLAR") == 0) {
-					psys->SetVelocityMode(CParticleSys::VelocityMode::MODE_POLAR);
-				} else if (right->compare("RECTANGULAR") == 0) {
-					psys->SetVelocityMode(CParticleSys::VelocityMode::MODE_RECT);
-				} else {
-					loadError = L"Unrecognized velocity mode";
-					return false;
-				}
-
-			} else if (left->compare("MinVelocity") == 0) {
-				double min, max;
-				psys->GetVelocity(&min, &max);
-				parseRight >> min;
-				psys->SetVelocity(min, max);
-
-			} else if (left->compare("MaxVelocity") == 0) {
-				double min, max;
-				psys->GetVelocity(&min, &max);
-				parseRight >> max;
-				psys->SetVelocity(min, max);
-
-			} else if (left->compare("MinVelocityX") == 0) {
-				double min, max;
-				psys->GetRectVelocityX(&min, &max);
-				parseRight >> min;
-				psys->SetRectVelocityX(min, max);
-
-			} else if (left->compare("MaxVelocityX") == 0) {
-				double min, max;
-				psys->GetRectVelocityX(&min, &max);
-				parseRight >> max;
-				psys->SetRectVelocityX(min, max);
-
-			} else if (left->compare("MinVelocityY") == 0) {
-				double min, max;
-				psys->GetRectVelocityY(&min, &max);
-				parseRight >> min;
-				psys->SetRectVelocityY(min, max);
-
-			} else if (left->compare("MaxVelocityY") == 0) {
-				double min, max;
-				psys->GetRectVelocityY(&min, &max);
-				parseRight >> max;
-				psys->SetRectVelocityY(min, max);
-
-			} else if (left->compare("AccelerationX") == 0) {
-				double x, y;
-				psys->GetAcceleration(&x, &y);
-				parseRight >> x;
-				psys->SetAcceleration(x, y);
-
-			} else if (left->compare("AccelerationY") == 0) {
-				double x, y;
-				psys->GetAcceleration(&x, &y);
-				parseRight >> y;
-				psys->SetAcceleration(x, y);
-
-			} else if (left->compare("MaximumAge") == 0) {
-				double maxAge;
-				parseRight >> maxAge;
-				psys->SetMaxAge(maxAge);
-
-			} else if (left->compare("EmissionRate") == 0) {
-				double emissionRate;
-				parseRight >> emissionRate;
-				psys->SetEmissionRate(emissionRate);
-
-			} else if (left->compare("EmissionRadius") == 0) {
-				double emissionRadius;
-				parseRight >> emissionRadius;
-				psys->SetEmissionRadius(emissionRadius);
-
-			} else if (left->compare("GradientStep") == 0) {
-				std::string *left2;
-				std::string *right2;
-				if (SplitString(*right, ',', &left2, &right2)) {
-					if (!includesGradient) {
-						gradient.DeleteAllSteps();
-						includesGradient = true;
+				if (left->compare("VelocityMode") == 0) {
+					if (right->compare("POLAR") == 0) {
+						psys->SetVelocityMode(CParticleSys::VelocityMode::MODE_POLAR);
+					} else if (right->compare("RECTANGULAR") == 0) {
+						psys->SetVelocityMode(CParticleSys::VelocityMode::MODE_RECT);
+					} else {
+						loadError = L"Unrecognized velocity mode";
+						return false;
 					}
-					std::istringstream parseLeft2(*left2);
-					std::istringstream parseRight2(*right2);
-					double pos;
-					COLORREF color;
-					parseLeft2 >> pos;
-					parseRight2 >> std::hex >> color;
-					gradient.AddStep(pos, color);
-				} else {
-					loadError = L"GradientStep without ','";
-					return false;
+
+				} else if (left->compare("MinVelocity") == 0) {
+					double min, max;
+					psys->GetVelocity(&min, &max);
+					parseRight >> min;
+					psys->SetVelocity(min, max);
+
+				} else if (left->compare("MaxVelocity") == 0) {
+					double min, max;
+					psys->GetVelocity(&min, &max);
+					parseRight >> max;
+					psys->SetVelocity(min, max);
+
+				} else if (left->compare("MinVelocityX") == 0) {
+					double min, max;
+					psys->GetRectVelocityX(&min, &max);
+					parseRight >> min;
+					psys->SetRectVelocityX(min, max);
+
+				} else if (left->compare("MaxVelocityX") == 0) {
+					double min, max;
+					psys->GetRectVelocityX(&min, &max);
+					parseRight >> max;
+					psys->SetRectVelocityX(min, max);
+
+				} else if (left->compare("MinVelocityY") == 0) {
+					double min, max;
+					psys->GetRectVelocityY(&min, &max);
+					parseRight >> min;
+					psys->SetRectVelocityY(min, max);
+
+				} else if (left->compare("MaxVelocityY") == 0) {
+					double min, max;
+					psys->GetRectVelocityY(&min, &max);
+					parseRight >> max;
+					psys->SetRectVelocityY(min, max);
+
+				} else if (left->compare("AccelerationX") == 0) {
+					double x, y;
+					psys->GetAcceleration(&x, &y);
+					parseRight >> x;
+					psys->SetAcceleration(x, y);
+
+				} else if (left->compare("AccelerationY") == 0) {
+					double x, y;
+					psys->GetAcceleration(&x, &y);
+					parseRight >> y;
+					psys->SetAcceleration(x, y);
+
+				} else if (left->compare("MaximumAge") == 0) {
+					double maxAge;
+					parseRight >> maxAge;
+					psys->SetMaxAge(maxAge);
+
+				} else if (left->compare("EmissionRate") == 0) {
+					double emissionRate;
+					parseRight >> emissionRate;
+					psys->SetEmissionRate(emissionRate);
+
+				} else if (left->compare("EmissionRadius") == 0) {
+					double emissionRadius;
+					parseRight >> emissionRadius;
+					psys->SetEmissionRadius(emissionRadius);
+
+				} else if (left->compare("GradientStep") == 0) {
+					std::string *left2;
+					std::string *right2;
+					if (SplitString(*right, ',', &left2, &right2)) {
+						if (!includesGradient) {
+							gradient.DeleteAllSteps();
+							includesGradient = true;
+						}
+						std::istringstream parseLeft2(*left2);
+						std::istringstream parseRight2(*right2);
+						double pos;
+						COLORREF color;
+						parseLeft2 >> pos;
+						parseRight2 >> std::hex >> color;
+						gradient.AddStep(pos, color);
+					} else {
+						loadError = L"GradientStep without ','";
+						return false;
+					}
 				}
+
+				delete left, right;
+			}
+		} else {
+			// bitmapRow is not -1; read a line of the bitmap
+			if (line.length() < 30) {
+				loadError = L"Unexpected end of line while reading bitmap data";
+				return false;
 			}
 
-			delete left, right;
+			for (int i = 0; i < 30; i++) {
+				PatBlt(particleBmpDC, i, bitmapRow, 1, 1, (line[i] == '#') ? BLACKNESS : WHITENESS);
+			}
+
+			if (++bitmapRow > 4) bitmapRow = -1;
 		}
 	}
 	return true;
