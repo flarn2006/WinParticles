910790cfaaf4ba6a35ab7727eefba52e55ce4d00
flarn2006 <flarn2006@gmail.com>
2015-01-07 16:57:13 -0500

    Changed MultiplyColors to use integer math

--- WinParticles/Common.cpp
+++ WinParticles/Common.cpp
@@ -10,10 +10,10 @@ double Interpolate(double value, double inputMin, double inputMax, double output
 
 COLORREF MultiplyColors(COLORREF a, COLORREF b)
 {
-	float red1 = (float)GetRValue(a) / 255, red2 = (float)GetRValue(b) / 255;
-	float green1 = (float)GetGValue(a) / 255, green2 = (float)GetGValue(b) / 255;
-	float blue1 = (float)GetBValue(a) / 255, blue2 = (float)GetBValue(b) / 255;
-	return RGB((BYTE)(255.0 * red1 * red2), (BYTE)(255.0 * green1 * green2), (BYTE)(255.0 * blue1 * blue2));
+	int red1 = GetRValue(a), red2 = GetRValue(b);
+	int green1 = GetGValue(a), green2 = GetGValue(b);
+	int blue1 = GetBValue(a), blue2 = GetBValue(b);
+	return RGB(red1 * red2 / 255, green1 * green2 / 255, blue1 * blue2 / 255);
 }
 
 double RandInRange(double min, double max)
