b3769d5bef4bab040c9c411eb1e0699e32e36813
flarn2006 <flarn2006@gmail.com>
2015-04-21 00:37:32 -0400

    "Scale entire system" now accepts numeric entry by pressing Enter

--- WinParticles/ScaleAllTextItem.cpp
+++ WinParticles/ScaleAllTextItem.cpp
@@ -1,9 +1,20 @@
 #include <cmath>
 #include "stdafx.h"
 #include "ScaleAllTextItem.h"
+#include "RootDisplay.h"
+#include "NumericInputBox.h"
+
+extern CRootDisplay *display;
 
 CScaleAllTextItem::CScaleAllTextItem()
 {
+}
+
+void CScaleAllTextItem::ScaleAllBy(double factor)
+{
+	for (auto i = affectedParams.begin(); i != affectedParams.end(); i++) {
+		(*i)->SetValue(factor * (*i)->GetValue());
+	}
 }
 
 void CScaleAllTextItem::GetItemText(tstring &text) const
@@ -14,12 +25,17 @@ void CScaleAllTextItem::GetItemText(tstring &text) const
 void CScaleAllTextItem::OnMouseWheel(short wheelDelta)
 {
 	double multiplier = std::pow(2.0, (double)wheelDelta / WHEEL_DELTA / 16);
-	for (auto i = affectedParams.begin(); i != affectedParams.end(); i++) {
-		(*i)->SetValue(multiplier * (*i)->GetValue());
-	}
+	ScaleAllBy(multiplier);
 }
 
 void CScaleAllTextItem::AddParam(CAnimTarget<double> *param)
 {
 	affectedParams.push_back(param);
+}
+
+void CScaleAllTextItem::OnEnterKey()
+{
+	display->GetNumInputBox()->PromptForValue((CNumericInputBox::Callback)[this](double value) {
+		ScaleAllBy(value);
+	}, TEXT("Scale by factor:"));
 }
--- WinParticles/ScaleAllTextItem.h
+++ WinParticles/ScaleAllTextItem.h
@@ -7,10 +7,12 @@ class CScaleAllTextItem : public CSelectableTextItem
 {
 private:
 	std::vector<CAnimTarget<double>*> affectedParams;
+	void ScaleAllBy(double factor);
 
 protected:
 	virtual void GetItemText(tstring &text) const;
 	virtual void OnMouseWheel(short wheelDelta);
+	virtual void OnEnterKey();
 
 public:
 	CScaleAllTextItem();
